715282ee370f11ea9ee538356a799c4d7c75ec70
Eva <evacherkashyna9@gmail.com>
2026-01-21 22:12:44 +0200

    Update admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,7 +10,31 @@ class BookInline(admin.TabularInline):
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'last_name', 'birth_date' )
-    search_fields = ('last_name', 'first_name')
+    list_display = ('name',
+                    'last_name',
+                    'birth_date' )
+
+    search_fields = ('last_name', 'name')
     search_help_text = 'Пошук за ім/ям та прізвищем '
+    inlines = (BookInline,)
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('author',
+                    'genre',
+                    'name',
+                    'is_available',
+                    'book_info')
+    list_filter=('author','is_available', 'genre')
+    @admin.display(description='Инфо')
+    def book_info(self, obj:Book):
+        if len(obj.description) > 100:
+            return "- Довгий опис"
+        return "- Короткий опис"
+
+
+
+
+
 
