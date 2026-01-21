555518f51b14fbae48234bd93d61d2a1fc35550d
Eva <evacherkashyna9@gmail.com>
2026-01-21 22:38:05 +0200

    Enhance Author and Book admin configurations
    Added search fields, display links, and fieldsets to BookAdmin. Introduced actions for availability status.

--- catalog/admin.py
+++ catalog/admin.py
@@ -12,8 +12,8 @@ class BookInline(admin.TabularInline):
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('name',
                     'last_name',
-                    'birth_date' )
-
+                    'birth_date')
+    list_display_links = ('last_name', 'name')
     search_fields = ('last_name', 'name')
     search_help_text = 'Пошук за ім/ям та прізвищем '
     inlines = (BookInline,)
@@ -26,15 +26,34 @@ class BookAdmin(admin.ModelAdmin):
                     'name',
                     'is_available',
                     'book_info')
-    list_filter=('author','is_available', 'genre')
+    list_filter = ('author', 'genre')
+    search_fields = ('name', 'genre')
+    search_help_text = 'Пошук за назвою книги або жанром'
+
+    fieldsets = (
+        ('Основна інфа', {
+            'fields': ('name', 'author', 'genre')
+        }),
+        ('Додатково', {
+            'fields': ('description', 'pages_count', 'is_available', 'created_at'),
+            'classes': ('collapse',),
+        }),
+    )
+
     @admin.display(description='Инфо')
-    def book_info(self, obj:Book):
+    def book_info(self, obj: Book):
         if len(obj.description) > 100:
-            return "- Довгий опис"
-        return "- Короткий опис"
+            return '> Довгий опис'
+        return '> Короткий опис'
 
+    actions = ['make_available', 'make_unavailable']
 
+    @admin.action(description='(В наявності)')
+    def make_available(self, request, queryset):
+        updated = queryset.update(is_available=True)
+        self.message_user(request, f'Оновлено {updated}')
 
-
-
-
+    @admin.action(description='(Немає в наявності)')
+    def make_unavailable(self, request, queryset):
+        updated = queryset.update(is_available=False)
+        self.message_user(request, f'Оновлено {updated}')
